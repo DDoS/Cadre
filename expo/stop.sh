9509e7f42b0deb023b7864b70ad46069bc9518ab
Aleksi Sapon <aleksi@sapon.ca>
2024-11-30 15:00:04 -0500

    Custom Expo post command

--- expo/stop.sh
+++ expo/stop.sh
@@ -1,4 +1,15 @@
-pid=$(lsof -i :21110 | awk '/flask|Python/ { pid=$2 } END { print pid }')
+Port=21110
+while getopts "p::" option; do
+   case $option in
+      p)
+         Port=$OPTARG;;
+     \?)
+         echo "invalid option"
+         exit;;
+   esac
+done
+
+pid=$(lsof -i :$Port | awk '/flask|Python/ { pid=$2 } END { print pid }')
 if [ -n "$pid" ]; then
     kill -2 "$pid"
     echo "Sent SIGINT to $pid"
